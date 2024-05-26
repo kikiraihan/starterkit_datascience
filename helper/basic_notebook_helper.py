b15ab80698db158872355712ff5df08d9ec6916a
Moh Zulkifli Katili <mohzulkiflikatili@gmail.com>
2024-05-26 13:25:48 +0800

    Update and rename notebook_import_from_root.py to basic_notebook_helper.txt

---
+++ helper/basic_notebook_helper.py
@@ -0,0 +1,8 @@
+# change root folder in notebook
+import sys
+sys.path.append('../')
+from helper.visualisasiHelper import plotly_graph
+
+# import module reload
+import importlib
+importlib.reload(sys.modules['folder_modul.nama_modul'])
--- helper/notebook_import_from_root.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.append('../')
-from helper.visualisasiHelper import plotly_graph
