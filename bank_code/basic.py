652c70c9d21d775279a156d38d856a400b251a6c
Moh Zulkifli Katili <mohzulkiflikatili@gmail.com>
2024-06-05 20:47:00 +0800

    Update basic.py

--- bank_code/basic.py
+++ bank_code/basic.py
@@ -6,3 +6,7 @@ from helper.visualisasiHelper import plotly_graph
 # import module reload
 import importlib
 importlib.reload(sys.modules['folder_modul.nama_modul'])
+
+
+#reverse dataframe
+df_reversed = df.iloc[::-1]
